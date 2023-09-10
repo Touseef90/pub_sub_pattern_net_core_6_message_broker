64bc80569d585203a9e28160f6269ee5d2bc4cfa
Touseef90 <touseef.ghazanfar@gmail.com>
2023-09-10 12:45:04 +0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+using MessageBroker.Data;
+using MessageBroker.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlite("Data Source=MessageBroker.db"));
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+// Create Topic
+app.MapPost("api/topics", async (AppDbContext context, Topic topic) =>
+{
+    await context.Topics.AddAsync(topic);
+
+    await context.SaveChangesAsync();
+
+    return Results.Created($"api/topics/{topic.Id}", topic);
+});
+
+// Return all topics
+app.MapGet("api/topics", async (AppDbContext context) =>
+{
+    var topics = await context.Topics.ToListAsync();
+
+    return Results.Ok(topics);
+});
+
+app.Run();
